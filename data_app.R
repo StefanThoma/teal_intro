d5b8575dedc85f888acb8c717950ade99dd028f0
StefanThoma <40463122+StefanThoma@users.noreply.github.com>
2022-12-16 16:44:52 +0100

    cicd added

---
+++ data_app.R
@@ -0,0 +1,37 @@
+# data app
+## ---- load packages data app ----
+library(random.cdisc.data) # this package provides the data used
+library(teal.data)
+library(scda)
+library(teal.modules.general)
+library(sparkline)
+library(teal)
+library(tidyverse)
+
+## ---- parameter data app ----
+author <- "Stefan Thoma"
+molecule <- "hexadromedar"
+
+
+## ---- load data data app ----
+ADSL <- random.cdisc.data::cadsl
+ADAE <- random.cdisc.data::cadae
+ADTTE <- random.cdisc.data::cadtte
+
+
+## ---- init data app ----
+app <- teal::init(
+  data = cdisc_data(
+    cdisc_dataset("ADSL", ADSL) %>%
+      mutate_dataset(script = "ADSL_preproc.R"),
+    cdisc_dataset("ADAE", ADAE),
+    cdisc_dataset("ADTTE", ADTTE)
+  ),
+  modules = modules(tm_variable_browser(label = "View Variables")),
+  header = "My first application",
+  footer = "footer"
+)
+
+
+## ---- run data app ----
+shinyApp(app$ui, app$server)
