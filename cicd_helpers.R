3a289d657fc6d41e116aeed0b8e4e4c2233ebd82
StefanThoma <stefan.thoma@gmail.com>
2022-12-16 22:55:39 +0100

    try cicd

--- cicd_helpers.R
+++ cicd_helpers.R
@@ -1,10 +1,5 @@
 library(spelling)
-spelling::spell_check_files(list.files()[endsWith(list.files(), "md")])
+words <- spelling::spell_check_files(list.files()[endsWith(list.files(), "md")], ignore = read.csv("inst/WORDLIST")[[1]])
 
 library(tidyverse)
 map(list.files()[endsWith(list.files(), "md")], lintr::lint)
-
-
-devtools::install_github("r-lib/covr")
-
-
