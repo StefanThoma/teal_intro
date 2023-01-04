d3c091c916bf6435c2b080404d776ff654c952f1
StefanThoma <stefan.thoma@gmail.com>
2023-01-04 22:34:43 +0100

    data added modules 1

--- R/data_app.R
+++ R/data_app.R
@@ -9,14 +9,37 @@ library(teal)
 library(tidyverse)
 
 ## ---- parameter data app ----
+# metadata
 author <- "Stefan Thoma"
 molecule <- "hexadromedar"
 
+# parameters
+demographic_variables <- c("SEX", "AGE", "RACE")
+arm_variable <- "ARM"
 
 ## ---- load data data app ----
 ADSL <- random.cdisc.data::cadsl
 ADAE <- random.cdisc.data::cadae
 ADTTE <- random.cdisc.data::cadtte
+
+
+modules(
+  tm_front_page(
+    label = "Study Information",
+    header_text = c("Info about data source" = "Random data are used that have been created with the 'scda' R package"),
+    tables = list(`NEST packages used` = data.frame(Packages = c("teal.modules.general", "teal.modules.clinical", "scda", "scda.2021")))
+  ),
+  tm_data_table("Data Table"),
+  tm_variable_browser("Variable Browser"),
+  tm_t_summary(
+    label = "Demographic Table",
+    dataname = "ADSL",
+    arm_var = amr_variable,
+    summarize_vars = choices_selected(
+      choices = variable_choices(ADSL, demographic_variables),
+      selected = c("SEX", "AGE", "RACE")
+    )
+  )
 
 
 ## ---- init data app ----
@@ -27,6 +50,26 @@ app <- teal::init(
     cdisc_dataset("ADAE", ADAE),
     cdisc_dataset("ADTTE", ADTTE)
   ),
+  modules = modules(
+    tm_front_page(
+      label = "Study Information",
+      header_text = 
+        c("Info about data source" = "Random data are used that have been created with the 'scda' R package"),
+      tables = 
+        list("NEST packages used" = 
+               data.frame(Packages = 
+                            c("teal.modules.general", "teal.modules.clinical", "scda", "scda.2021")))),
+    tm_data_table("Data Table"),
+    tm_variable_browser("Variable Browser"),
+    tm_t_summary(
+      label = "Demographic Table",
+      dataname = "ADSL",
+      arm_var = cs_arm_var,
+      summarize_vars = choices_selected(
+        choices = variable_choices(ADSL, demog_vars_asl),
+        selected = c("SEX", "AGE", "RACE")
+      )
+    )),
   header = "My first application",
   footer = "footer"
 )
@@ -34,4 +77,3 @@ app <- teal::init(
 
 ## ---- run data app ----
 shinyApp(app$ui, app$server)
-?teal.modules.clinical::tm_g_forest_tte
